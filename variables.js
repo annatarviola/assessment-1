9ae12d82bb38ea26e1a08c78172db12e8dfc62c0
Lily Springer <lilyspringer@hotmail.com>
2023-01-22 15:50:57 -0700

    variables.js problems completed

--- variables.js
+++ variables.js
@@ -1,17 +1,17 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
-//CODE HERE 
+var myName = "Lily"
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
-//CODE HERE
+let faveNum = 13
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
-//CODE HERE
+let lovesCode = true
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -20,11 +20,19 @@
   If it's not, console.log('not lucky 13').
 */
 
-//CODE HERE
+// (lol 13 is really just my favorite number and I had it there before I read this question)
+
+if (faveNum = 13) {
+  console.log('lucky 13!')
+} else {
+  console.log('not lucky 13')
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
 */
 
-//CODE HERE
+for (let i = 0; i < 5; i++) {
+  console.log(faveNum)
+}
