88772a6a42ca6923c62b4f27fce378de164a4c43
Lily Springer <lilyspringer@hotmail.com>
2023-01-22 15:45:13 -0700

    sdlc problem 2 finished

--- sdlc.js
+++ sdlc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Implementation
-// Design
-// Maintain
-// Analysis of Requirements
-// Testing & Integration
 // Planning
+// Analysis of Requirements
+// Design 
+// Implementation
+// Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -18,4 +18,9 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const analysisOfRequirements = "This is the second step, wherein a senior team member or project member outlines what the project will necessitate in terms of approach, resources, and mechanics, and in accordance with business needs"
+const design = "Based on the analysis, the technical details of the project will be defined by developers"
+const implementation = "The system will be constructed and brought into operation based on the design"
+const testingAndIntegration = "The system will undergo tests to ensure functionality before being presented as a working program"
+const maintain = "Maintenance will be upkept to ensure the program continues running functionally"
